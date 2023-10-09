ac9181862c504ee5d70591e86cfa96b9ea36518a
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-10-09 22:26:32 +0200

    Added Heading Component

--- src/components/BlockRenderer/BlockRenderer.tsx
+++ src/components/BlockRenderer/BlockRenderer.tsx
@@ -1,12 +1,23 @@
 import { Cover } from "../Cover";
-
+import { Heading } from "../Heading";
 export const BlockRenderer = ({ blocks }: any) => {
   return blocks.map((block: any) => {
     switch (block.name) {
+      case "core/heading": {
+        return (
+          <Heading
+            key={block.id}
+            textAlign={block.attributes.textAlign}
+            level={block.attributes.level}
+            content={block.attributes.content}
+          />
+        );
+      }
       case "core/cover": {
+        console.log("BLOCKS", block);
         return (
           <Cover key={block.id} background={block.attributes.url}>
-            core cover
+            <BlockRenderer blocks={block.innerBlocks} />
           </Cover>
         );
       }
