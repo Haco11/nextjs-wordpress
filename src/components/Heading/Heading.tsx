c708911102710bba6c66d8a47e1900c0ead84b40
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-10-10 13:29:13 +0200

    Fixed styling on heading

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,10 +1,10 @@
 import { getFontSizeForHeading, getTextAlign } from "@/utils/fonts";
 import React from "react";
 
-export const Heading = ({ textAlign, content, level }: any) => {
+export const Heading = ({ textAlign, content, level = 2 }: any) => {
   const tag = React.createElement(`h${level}`, {
     dangerouslySetInnerHTML: { __html: content },
-    className: `font-heading max-w-5xl mx-auto my-5 ${getFontSizeForHeading(
+    className: `font-heading max-w-3xl mx-auto my-5 ${getFontSizeForHeading(
       level
     )} ${getTextAlign(textAlign)}`,
   });
