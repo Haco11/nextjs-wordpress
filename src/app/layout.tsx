a0f788109af7ba6ef53766ee58271eed21c1a651
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-10-13 14:21:59 +0200

    Changed Mainmenu from pages to layout and added new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
+import { MainMenu } from "@/components/MainMenu";
 import "../styles/globals.css";
-export default function RootLayout({
+import { getPage } from "@/utils/base";
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const data = await getPage("/");
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="font-body">
+        <MainMenu items={data?.mainMenuItems} />
+
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { getPage } from "@/utils/base";
 import { BlockRenderer } from "@/components/BlockRenderer";
-import { MainMenu } from "@/components/MainMenu";
 export default async function Home() {
   const data = await getPage("/");
   return (
     <main>
-      <MainMenu items={data?.mainMenuItems} />
       <BlockRenderer blocks={data?.blocks} />
     </main>
   );
