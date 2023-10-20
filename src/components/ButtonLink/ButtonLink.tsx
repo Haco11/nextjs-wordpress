40902a6f34133ba5ea19493d0223685979445342
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-10-20 13:42:04 +0200

    fixed styling

--- src/components/ButtonLink/ButtonLink.tsx
+++ src/components/ButtonLink/ButtonLink.tsx
@@ -2,9 +2,7 @@ import Link from "next/link";
 
 export const ButtonLink = ({ destination, label }: any) => {
   return (
-    <Link
-      href={destination}
-      className="bg-pink-500 hover:bg-pink-700 inline-block my-2 px-4 py-2 uppercase rounded-md cursor-pointer font-bold text-white">
+    <Link href={destination} className="btn">
       {label}
     </Link>
   );
